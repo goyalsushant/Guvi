3aac60899709424a71791cfa880a0e3f03c03d38
goyalsushant <32196834+goyalsushant@users.noreply.github.com>
2025-07-30 16:20:48 +0530

    Add files via upload

---
+++ 2. Javascript basics/console.js
@@ -0,0 +1,15 @@
+// let username = prompt('What is your name') // "sushant" or ""
+// if(username) {
+//     alert('Hello, ' + username)
+// }
+// else {
+//     alert('You did not enter your name.')
+// }
+
+let isConfirmed = confirm('Do you want to delete this item?') // true or false
+if(isConfirmed) {
+    alert('Item Deleted')
+}
+else {
+    alert('Item not deleted')
+}
