fb4c974c3361a09fae1b323a0dea83c958d4cc44
Sushant Goyal <32196834+goyalsushant@users.noreply.github.com>
2025-11-08 13:27:06 +0530

    Add files via upload

---
+++ ques.js
@@ -0,0 +1,36 @@
+// // // /**
+// // //  * c is an object with a key as greeting
+// // //  */
+// // // let c = { 
+// // //     greeting: 'Hey!',
+// // // };
+// // // let d;
+
+// // // d = c; // memory reference of the variable c is being copied to variable d
+// // // c.greeting = 'Hello';
+// // // console.log(d.greeting);
+
+// // class Chameleon {
+// //   static colorChange(newColor) {
+// //     this.newColor = newColor;
+// //     return this.newColor;
+// //   }
+
+// //   constructor({ newColor = 'green' } = {}) {
+// //     this.newColor = newColor;
+// //   }
+// // }
+
+// // const freddie = new Chameleon({ newColor: 'purple' });
+// // console.log(freddie.colorChange('orange'));
+
+// function bark() {
+//   console.log('Woof!');
+// }
+
+// console.log(typeof bark)
+// bark.animal = 'dog';
+// // console.log(typeof bark)
+// // console.log(bark.animal)
+
+// console.log(bark instanceof Object)
